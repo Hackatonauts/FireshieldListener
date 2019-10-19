35dc5d8451c70f81b80fd155ad34ec7d1d70c937
Balczynski <artur.balczynski@capgemini.com>
2019-10-19 16:07:43 +0200

    service rename

---
+++ src/main/java/hackatonauts/fireshield/listener/FireEventsListenerApplication.java
@@ -0,0 +1,24 @@
+package hackatonauts.fireshield.listener;
+
+import hackatonauts.fireshield.listener.model.FireEvent;
+import hackatonauts.fireshield.listener.model.FireEventSource;
+import hackatonauts.fireshield.listener.model.FireModel;
+import hackatonauts.fireshield.listener.model.FireResponse;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireEventsListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireEventsListenerApplication.class, args);
+		FireService service = new FireService();
+		FireModel model = service.getFireModel();
+		FireEvent event = model.getEvents().get(0);
+		FireResponse response = new FireResponse(event.getTitle(),
+												event.getGeometries()[0].getPosition(),
+												event.getGeometries()[0].getDate(),
+												new FireEventSource("EONET", event.getSourceId()), event.getConfidence());
+		System.out.println(response.toString());
+	}
+}
