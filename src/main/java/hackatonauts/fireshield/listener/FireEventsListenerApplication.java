a71294b510f385fbbdfafefcc7808f43d8a00499
Balczynski <artur.balczynski@capgemini.com>
2019-10-19 17:07:43 +0200

    add days filter

--- src/main/java/hackatonauts/fireshield/listener/FireEventsListenerApplication.java
+++ src/main/java/hackatonauts/fireshield/listener/FireEventsListenerApplication.java
@@ -7,18 +7,22 @@ import hackatonauts.fireshield.listener.model.FireResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class FireEventsListenerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FireEventsListenerApplication.class, args);
 		FireService service = new FireService();
-		FireModel model = service.getFireModel();
-		FireEvent event = model.getEvents().get(0);
-		FireResponse response = new FireResponse(event.getTitle(),
-												event.getGeometries()[0].getPosition(),
-												event.getGeometries()[0].getDate(),
-												new FireEventSource("EONET", event.getSourceId()), event.getConfidence());
+		FireModel model = service.getFireModel(10);
+		List<FireEvent> eventList = model.getEvents();
+		eventList.stream().forEach(e -> System.out.println(e.getGeometries()[0].getDate()));
+		FireResponse response = new FireResponse(eventList.get(0).getTitle(),
+                eventList.get(0).getGeometries()[0].getPosition(),
+                eventList.get(0).getGeometries()[0].getDate(),
+												new FireEventSource("EONET", eventList.get(0).getSourceId()), eventList.get(0).getConfidence());
 		System.out.println(response.toString());
-	}
+//        System.out.println(service.postFireResponse(response).toString());
+    }
 }
--- src/main/java/hackatonauts/fireshield/listener/helpers/Constants.java
+++ src/main/java/hackatonauts/fireshield/listener/helpers/Constants.java
@@ -3,5 +3,7 @@ package hackatonauts.fireshield.listener.helpers;
 public class Constants {
 
     public static String fireEvents = "https://eonet.sci.gsfc.nasa.gov/api/v2.1/categories/8";
+    public static String daysParameter = "?days=";
     public static String baseUrl = "http://192.168.137.1:8080";
+    public static String endpointName = "fire";
 }
