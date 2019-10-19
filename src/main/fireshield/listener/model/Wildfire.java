ef7ec98c3d9d390349ceb402357a2de64a52207d
Balczynski <artur.balczynski@capgemini.com>
2019-10-19 12:24:59 +0200

    Project create

---
+++ src/main/fireshield/listener/model/Wildfire.java
@@ -0,0 +1,17 @@
+package main.fireshield.listener.model;
+
+public class Wildfire {
+    private String soruceId;
+    private String sourceType;
+
+    private String date;
+    private String name;
+
+    private double latCoordinate;
+    private double longCoordinate;
+
+    @Override
+    public String toString() {
+        return
+    }
+}
