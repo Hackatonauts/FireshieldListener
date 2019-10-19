29a81bf0e13bf93e1c5f07b692f937b3dc7e1642
Balczynski <artur.balczynski@capgemini.com>
2019-10-19 17:11:22 +0200

    slash add

--- src/main/java/hackatonauts/fireshield/listener/helpers/Constants.java
+++ src/main/java/hackatonauts/fireshield/listener/helpers/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
 
     public static String fireEvents = "https://eonet.sci.gsfc.nasa.gov/api/v2.1/categories/8";
     public static String daysParameter = "?days=";
-    public static String baseUrl = "http://192.168.137.1:8080";
+    public static String baseUrl = "http://192.168.137.1:8080/";
     public static String endpointName = "fire";
 }
